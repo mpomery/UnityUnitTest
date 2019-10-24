d579783114740bd73b34fc117b353a11f33cd429
Mitchell Pomery <bob_george33@hotmail.com>
2019-10-24 17:12:18 +1100

    Unit and Integration Tests

---
+++ UnityUnitTest/Assets/UnitTests/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class UnitTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void UnitTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.IsTrue(false);
+        }
+    }
+}
