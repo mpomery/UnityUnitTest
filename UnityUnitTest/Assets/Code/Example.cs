65b938862db96b6ecc4a46afc9acf94c496f28e7
Mitchell Pomery <bob_george33@hotmail.com>
2019-10-24 16:26:41 +1100

    Unit Test Created

---
+++ UnityUnitTest/Assets/Code/Example.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (MethodToTest())
+        {
+            Debug.Log("Instatiate");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MethodToTest())
+        {
+            Debug.Log("Frame");
+        }
+    }
+
+    public static bool MethodToTest()
+    {
+        return true;
+    }
+}
---
+++ UnityUnitTest/Assets/UnitTests/ExampleTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class ExampleTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MethodToTestReturnsTrue()
+        {
+            Assert.IsTrue(Example.MethodToTest());
+        }
+    }
+}
